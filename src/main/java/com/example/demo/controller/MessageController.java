1af109a31defb4b9358fbe1d390392a38ec9b337
Eknath Take <eknathtake@gmail.com>
2024-05-11 07:04:08 -0400

    sendMessage api integrated with UI

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,25 +29,32 @@ public class MessageController {
 	@Autowired
 	private KafkaService service;
 
-	@PostMapping(value = "/sendMessage"/* , consumes = { MediaType.MULTIPART_FORM_DATA_VALUE } */)
-	public void sendMessage(@RequestParam(name = "senderNumber") String senderNumber,
-			@RequestParam(name = "kafkaKey") String kafkaKey,
-			@RequestParam(name = "recepientNumber") String recepientNumber,
-			@RequestParam(name = "message") String message, @RequestParam(name = "file") MultipartFile file)
+	@PostMapping(value = "/sendMessage"/* , consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE} */)
+	public void sendMessage(
+			@RequestParam(name = "senderNumber", value = "senderNumber", required = false) String senderNumber,
+			@RequestParam(name = "kafkaKey", value = "kafkaKey", required = false) String kafkaKey,
+			@RequestParam(name = "recepientNumber", value = "recepientNumber", required = false) String recepientNumber,
+			@RequestParam(name = "message", value = "message", required = false) String message,
+			@RequestPart(name = "file", value = "file", required = false) MultipartFile file)
 			throws IOException {
 		
-		Double uuid = Math.random();
+		Message msg = mapMessageRequest(senderNumber, kafkaKey, recepientNumber, message, Math.random());
+
+		service.writeFile(msg.getUuid(), file);
+		logger.info("Sending Message: {}", message);
+		service.send(kafkaKey, msg);
+		logger.info("Message Sent");
+	}
+
+	private Message mapMessageRequest(String senderNumber, String kafkaKey, String recepientNumber, String message,
+			Double uuid) {
 		Message msg = new Message();
 		msg.setUuid(uuid);
 		msg.setMessageBody(message);
 		msg.setSenderNumber(senderNumber);
 		msg.setRecepientNumber(recepientNumber);
 		msg.setKey(kafkaKey);
-
-		service.writeFile(uuid, file);
-		logger.info("Sending Message: {}", message);
-		service.send(kafkaKey, msg);
-		logger.info("Message Sent");
+		return msg;
 	}
 
 	@GetMapping("/readMessage")
